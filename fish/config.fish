a1d0475d91fe75a4db12a1d977ac8b0a55477ce5
Irrisorr <zakharkevichg@gmail.com>
2025-05-17 18:41:42 +0200

    update fish config for thefuck app

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,8 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# Created by `pipx` on 2025-05-15 18:52:29
+set PATH $PATH /home/irrisorr/.local/bin
+
+thefuck --alias | source
